f9e66b3c696b75e22dd57789567092714f41e0a9
Jackson Kearl <jkearl@mit.edu>
2016-08-15 20:17:39 -0400

    initial commit

--- www/js/private.js
+++
@@ -1,9 +0,0 @@
-angular.module('ngOpenBadge.private', [])
-
-.factory('OBPrivate', function() {
-    PrivateVariables = {}
-    PrivateVariables.APP_KEY = "secret!"
-    PrivateVariables.BASE_URL = "http://openbadgedev2.media.mit.edu/"
-    PrivateVariables.DEVICE_UUID = "browser-uuid"
-    return PrivateVariables
-})
