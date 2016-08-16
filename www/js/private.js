7dda0428b0e5bb5207f28802a7dd7ae6046a9e5a
Jackson Kearl <jkearl@mit.edu>
2016-08-15 20:26:17 -0400

    add config data

---
+++ www/js/private.js
@@ -0,0 +1,9 @@
+angular.module('ngOpenBadge.private', [])
+
+.factory('OBPrivate', function() {
+    var PrivateData = {
+        APP_KEY:"",
+        BASE_URL:"",
+    }
+    return PrivateData
+})
