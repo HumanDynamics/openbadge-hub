d1ac5888d283a2724bba1ea57cce50694a64409c
Jackson Kearl <jkearl@mit.edu>
2016-11-24 18:42:27 -0500

    Fix for Some CM 13's not being able to enable bluetooth then eventaully crashing

--- www/js/index.js
+++ www/js/index.js
@@ -390,7 +390,9 @@ mainPage = new Page("main",
         if (app.bluetoothInitialized) {
             // after bluetooth is disabled, it's automatically re-enabled.
             //this.beginRefreshData();
-            app.disableBluetooth();
+            if (device.version[0] == '4')
+              app.disableBluetooth();
+
         }
     },
     function onHide() {
@@ -1126,6 +1128,7 @@ app = {
         });
     },
     disableBluetooth: function() {
+
         app.watchdogEnd();
         app.stopScan();
         console.log("Disabling Bluetooth!");
