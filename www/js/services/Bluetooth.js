f61f89ba7257bc59de2a2719c75d72d0beee4183
Jordan Reedie <jjreedie@gmail.com>
2018-06-22 10:31:40 -0400

    fix occasional infinite loop when connecting to badges

--- www/js/services/Bluetooth.js
+++ www/js/services/Bluetooth.js
@@ -341,8 +341,10 @@ angular.module('ngOpenBadge.services').factory('OBSBluetooth', function($cordova
         if (CRITICAL_LOGGING) {
           console.log("Error connecting: ", address, error.message);
         }
-        // we failed to connect - recursively try again
-        BluetoothFactory.connect(badge);
+        // we failed to connect - d/c and recursively try again
+        return BluetoothFactory.endConnection(badge).then(function () {
+          return BluetoothFactory.connect(badge);
+        });
       },
       function(notif) {
         console.log("connection notification: ", notif.address, notif.status);
